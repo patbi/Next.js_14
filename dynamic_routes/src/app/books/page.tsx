fbe10e02c7939aa84d5496c128debf809c20f639
Patrick <biyagapatrick@gmail.com>
2024-11-17 16:49:20 +0100

    second update

---
+++ dynamic_routes/src/app/books/page.tsx
@@ -0,0 +1,10 @@
+export default function BookList() {
+	return (
+		<>
+			<h1>Book List</h1>
+			<h2>Book 1</h2>
+			<h2>Book 2</h2>
+			<h2>Book 3</h2>
+		</>
+	);
+}
---
+++ nested_dynamic_routes/src/app/books/[bookId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,12 @@
+export default function ReviewDetail({
+	params,
+}: {
+	params: {
+		bookId: string;
+		reviewId: string;
+	};
+}) {
+	return (
+		<h1>Review {params.reviewId} for {params.bookId}</h1>
+	);
+}
